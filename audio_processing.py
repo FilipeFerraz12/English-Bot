e07fee8f17a7de1f2af47e9f3cb11bbb6f4b119a
FilipeFerraz12 <pipeferraz1@gmail.com>
2025-06-04 12:35:20 -0300

    Update audio_processing.py

--- audio_processing.py
+++ audio_processing.py
@@ -4,7 +4,6 @@ from pydub import AudioSegment
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 def transcribe_audio(audio_file):
     client = speech.SpeechClient()
